89aa7a6ea7a02829f1262a2a59d4d8b2dd3a8c15
40412 <jasmin.franti@gmail.com>
2026-05-29 18:46:21 +0300

    5.6-5.7

--- bloglist-frontend/src/components/Blog.jsx
+++ bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,34 @@
+import { useState } from "react";
+
 export const Blog = ({ blog }) => {
+  const [viewDetails, setViewDetails] = useState(false);
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
   return (
-    <div>
-      {blog.title} | {blog.author}
-    </div>
+    <>
+      <div style={blogStyle}>
+        {blog.title} | {blog.author}{" "}
+        <button onClick={() => setViewDetails(!viewDetails)}>
+          {viewDetails ? "Hide" : "View"}
+        </button>
+        {viewDetails && (
+          <div>
+            <div>{blog.url}</div>
+            <div>
+              {blog.likes} <button>Like</button>
+            </div>
+
+            <div>{blog.user.name}</div>
+          </div>
+        )}
+      </div>
+    </>
   );
 };
