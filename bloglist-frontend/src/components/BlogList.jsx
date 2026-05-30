27b3d9ce4c5b0c894fd74518de00dc50664dd766
40412 <jasmin.franti@gmail.com>
2026-05-30 10:21:42 +0300

    5.9-5.10

--- bloglist-frontend/src/components/BlogList.jsx
+++ bloglist-frontend/src/components/BlogList.jsx
@@ -15,9 +15,11 @@ export const BlockList = ({ blogs, setBlogs }) => {
   return (
     <div>
       <h2>blogs</h2>
-      {blogs.map((blog) => (
-        <Blog key={blog.id} blog={blog} />
-      ))}
+      {[...blogs]
+        .sort((a, b) => b.likes - a.likes)
+        .map((blog) => (
+          <Blog key={blog.id} blog={blog} />
+        ))}
     </div>
   );
 };
